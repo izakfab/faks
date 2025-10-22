1563b658a15b90752bf6aee0a08c989d5f606d69
Izak <izakfabjan5@gmail.com>
2025-10-22 10:40:35 +0200

    solves 01_04

---
+++ Programiranje/DodatneNaloge/casovnaRazlikaII.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class casovnaRazlikaII {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int h1 = s.nextInt();
+		int m1 = s.nextInt();
+		int h2 = s.nextInt();
+		int m2 = s.nextInt();
+		int delta = (m2 - m1 < 0 ? m2 - m1: m2 - m1) + 60 * (h2 - h1);
+		System.out.printf("%d:%02d", delta / 60, delta % 60);
+	}
+}
