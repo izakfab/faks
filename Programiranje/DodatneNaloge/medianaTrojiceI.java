5ddee592cf02675eb4bebc43e29610f192fbcb2d
Izak Fabjan <izakfabjan5@gmail.com>
2025-10-22 23:18:57 +0200

    solves 01_06

---
+++ Programiranje/DodatneNaloge/medianaTrojiceI.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class medianaTrojiceI {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int prvo = s.nextInt();
+		int drugo = s.nextInt();
+		int tretje = s.nextInt();
+		System.out.println (prvo <= drugo ? (drugo <= tretje ? drugo : prvo <= tretje ? tretje : prvo) : (prvo <= tretje ? prvo : drugo <= tretje ? tretje : drugo));
+	}
+}
