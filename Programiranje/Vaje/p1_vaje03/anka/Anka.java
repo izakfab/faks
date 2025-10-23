7bc4b654168f4ee81218c3c619107123556864b1
Izak <izakfabjan5@gmail.com>
2025-10-23 13:31:04 +0200

    Solves V03_02

---
+++ Programiranje/Vaje/p1_vaje03/anka/Anka.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Anka {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		int d = s.nextInt();
+		for (int i = 1; i < 10; i += 2)
+			for (int j = m + 1; j < 10; j++)
+				for (int k = 0; k < 10; k += d)
+					System.out.printf("%d-%d-%d%n", i, j, k);
+		System.out.println(5 * (9 - m) * (10 / d + 1));
+	}
+}
