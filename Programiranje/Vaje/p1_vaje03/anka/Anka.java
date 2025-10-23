0cecdf54bc1dd2712053dd72803d77361e39ed2d
Izak <izakfabjan5@gmail.com>
2025-10-23 13:39:58 +0200

    solves V03_02

--- Programiranje/Vaje/p1_vaje03/anka/Anka.java
+++ Programiranje/Vaje/p1_vaje03/anka/Anka.java
@@ -9,6 +9,6 @@ public class Anka {
 			for (int j = m + 1; j < 10; j++)
 				for (int k = 0; k < 10; k += d)
 					System.out.printf("%d-%d-%d%n", i, j, k);
-		System.out.println(5 * (9 - m) * (10 / d + 1));
+		System.out.println(5 * (9 - m) * (9 / d + 1));
 	}
 }
